bac467862ccf569e28688da30f6f6dfdae67d51a
John Park <kyopark2014@gmail.com>
2024-12-31 12:19:01 +0900

    update

--- cdk-endpoint/lib/cdk-endpoint-stack.ts
+++ cdk-endpoint/lib/cdk-endpoint-stack.ts
@@ -42,7 +42,7 @@ export class CdkEndpointStack extends cdk.Stack {
       vpcName: `vpc-for-${projectName}`,
       maxAzs: 2,
       ipAddresses: ec2.IpAddresses.cidr("10.64.0.0/16"),
-      natGateways: 0,
+      natGateways: 1,
       createInternetGateway: true,
       subnetConfiguration: [
         {
